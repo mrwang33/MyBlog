850d45d595c0c8f70ce755f81a813d23cee29a5b
wang <wang1386528@gmail.com>
2017-04-22 23:39:33 +0800

    //TODO

--- MyBlog/src/main/java/com/mb/action/MainAction.java
+++ MyBlog/src/main/java/com/mb/action/MainAction.java
@@ -9,11 +9,15 @@ public class MainAction {
 
 	@RequestMapping("/about")
 	public String about() {
+		//TODO 把相关东西查好
 		return "about";
 	}
 	
 	@RequestMapping("/leaveMsg")
 	public String leaveMsg() {
+		//TODO 把相关东西查好
 		return "leaveMsg";
 	}
+	
+	
 }
